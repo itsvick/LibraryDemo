a73a0264ff6da4d7acaf22e2d862772465ab89ed
sri-pusuluri <srini@zool.in>
2019-10-17 18:16:23 +0530

    creted content play componenet for the course content player for TOC

---
+++ src/app/desktop/content-play/content-play.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-play',
+  templateUrl: './content-play.component.html',
+  styleUrls: ['./content-play.component.scss']
+})
+export class ContentPlayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
